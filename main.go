28c9d52a4e3a3c0f879afac30c227ef779fda427
theonefirst <owen.hadi10@gmail.com>
2021-06-28 23:52:34 +0700

    Park a Car API

--- main.go
+++ main.go
@@ -65,6 +65,45 @@ func createParkingLot(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func allocateCarToParkingSpot(w http.ResponseWriter, r *http.Request) {
+	var newCarParked Car
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly input Car Object")
+		return
+	}
+
+	json.Unmarshal(reqBody, &newCarParked)
+	if parkingLot.TotalSlot == 0 {
+		fmt.Fprintf(w, "Parking lot has not been created yet")
+		return
+	} else if parkingLot.TotalSlot == parkingLot.OccupiedSlot {
+		fmt.Fprintf(w, "Parking lot is full")
+		return
+	} else {
+		for i := 0; i < len(parkingLot.ParkingSlot); i++ {
+			if parkingLot.ParkingSlot[i].SlotNo == parkingLot.NextFreeSlotNo {
+				parkingLot.ParkingSlot[i].Car = &Car{
+					LicenseNo: newCarParked.LicenseNo,
+					Colour:    newCarParked.Colour,
+				}
+				parkingLot.OccupiedSlot = parkingLot.OccupiedSlot + 1
+
+				for j := parkingLot.NextFreeSlotNo; j < len(parkingLot.ParkingSlot); j++ {
+					if parkingLot.ParkingSlot[j].Car == nil {
+						parkingLot.NextFreeSlotNo = j
+						break
+					}
+				}
+				w.WriteHeader(http.StatusCreated)
+				json.NewEncoder(w).Encode(newCarParked)
+				return
+			}
+		}
+	}
+
+}
+
 func getParkingLot(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(parkingLot)
 }
@@ -79,5 +118,6 @@ func main() {
 
 	router.HandleFunc("/parking-lot", createParkingLot).Methods("POST")
 	router.HandleFunc("/parking-lot", getParkingLot).Methods("GET")
+	router.HandleFunc("/parking-a-car", allocateCarToParkingSpot).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
